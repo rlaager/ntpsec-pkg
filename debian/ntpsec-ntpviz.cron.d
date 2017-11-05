500c1e8fced0d8b1c4db561f8c7a21268c0f1819
Richard Laager <rlaager@wiktel.com>
2017-11-05 04:15:29 -0600

    Work-in-progress 0.9.7~git20161217+dfsg1-1ubuntu1

--- debian/ntpsec-ntpviz.cron.d
+++ debian/ntpsec-ntpviz.cron.d
@@ -1,2 +1,2 @@
-53 *     * * * root ntpviz -p 1 -o /var/lib/ntp/ntpviz/day  2> /dev/null ; find /var/lib/ntp/ntpviz/day  -type f -mtime +1 -delete
-45 11,23 * * * root ntpviz -p 7 -o /var/lib/ntp/ntpviz/week 2> /dev/null ; find /var/lib/ntp/ntpviz/week -type f -mtime +7 -delete
+53 *     * * * root cd /var/lib/ntp/ntpviz && ntpviz @/etc/ntpviz/day/optionfile  2> /dev/null ; find /var/lib/ntp/ntpviz/day  -type f -mtime +1 -delete
+45 11,23 * * * root cd /var/lib/ntp/ntpviz && ntpviz @/etc/ntpviz/week/optionfile 2> /dev/null ; find /var/lib/ntp/ntpviz/week -type f -mtime +7 -delete
