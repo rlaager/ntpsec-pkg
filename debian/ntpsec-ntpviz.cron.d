cba7b0dc07dd8e3c41cef948b641ca209008b451
Richard Laager <rlaager@wiktel.com>
2017-11-08 02:43:01 -0600

    Use the ntpviz systemd units

--- debian/ntpsec-ntpviz.cron.d
+++ debian/ntpsec-ntpviz.cron.d
@@ -1,2 +1,2 @@
-53 *     * * * root ntpviz -p 1 -o /var/lib/ntp/ntpviz/day  @/etc/ntpviz/options 2> /dev/null ; find /var/lib/ntp/ntpviz/day  -type f -mtime +1 -delete
-45 11,23 * * * root ntpviz -p 7 -o /var/lib/ntp/ntpviz/week @/etc/ntpviz/options 2> /dev/null ; find /var/lib/ntp/ntpviz/week -type f -mtime +7 -delete
+53 *     * * * root if [ ! -d /run/systemd/system ] ; then ntpviz -p 1 -o /var/lib/ntp/ntpviz/day  @/etc/ntpviz/options 2> /dev/null ; find /var/lib/ntp/ntpviz/day  -type f -mtime +1 -delete ; fi
+45 11,23 * * * root if [ ! -d /run/systemd/system ] ; then ntpviz -p 7 -o /var/lib/ntp/ntpviz/week @/etc/ntpviz/options 2> /dev/null ; find /var/lib/ntp/ntpviz/week -type f -mtime +7 -delete ; fi
