b5ca87e8857d645f17d42451ea84db8a8e3c768c
Richard Laager <rlaager@wiktel.com>
2017-11-10 15:43:47 -0600

    Revert "Do not install ntploggps"
    This reverts commit ba31bc785345aca48cf66f32ec75fc0aa4d7a9ae.

--- debian/ntpsec-ntpviz.cron.d
+++ debian/ntpsec-ntpviz.cron.d
@@ -1,3 +1,4 @@
 53 *     * * * root if [ ! -d /run/systemd/system ] ; then ntpviz -p 1 -o /var/lib/ntp/ntpviz/day  @/etc/ntpviz/options 2> /dev/null ; find /var/lib/ntp/ntpviz/day  -type f -mtime +1 -delete ; fi
 45 11,23 * * * root if [ ! -d /run/systemd/system ] ; then ntpviz -p 7 -o /var/lib/ntp/ntpviz/week @/etc/ntpviz/options 2> /dev/null ; find /var/lib/ntp/ntpviz/week -type f -mtime +7 -delete ; fi
+*/5      * * * root [ ! -d /run/systemd/system ] && [ -e /run/gpsd.sock ] && [ -x /usr/sbin/ntploggps ] && /usr/sbin/ntploggps -o -l /var/log/ntpstats/gpsd 2> /dev/null
 */5      * * * root [ ! -d /run/systemd/system ] && [ -x /usr/sbin/ntplogtemp ] && /usr/sbin/ntplogtemp -o -l /var/log/ntpstats/temps
